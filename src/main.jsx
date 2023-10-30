3a085cea880a76c6023378eee063c9c31c41f380
Madara909 <115081897+Madara909@users.noreply.github.com>
2023-10-30 22:55:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+// main jsx file imports components from other jsx files
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
